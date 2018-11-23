fb2b64baa264ec2306d6c1dfec72e6be6d78862f
K00215515 <K00215515@student.lit.ie>
2018-11-23 14:28:10 +0000

    Adding pages

---
+++ Rythm Slasher/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    public Text countText;
+    private Rigidbody rb;
+    private int count;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+
+    }
+	
+	void Update () {
+		
+	}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            
+        }
+    }
+
+    public void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count == 10)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+
+    }
+}
